9668c0cd0a33906fab13d67e97e4ef7c7e3a84ff
Matteo Collina <matteo.collina@gmail.com>
2013-12-04 12:25:50 +0100

    Adapted example 2.

--- 2_function_arguments/addon.cc
+++ 2_function_arguments/addon.cc
@@ -1,24 +1,24 @@
-#define BUILDING_NODE_EXTENSION
 #include <node.h>
+#include "nan.h"
 
 using namespace v8;
 
-Handle<Value> Add(const Arguments& args) {
-  HandleScope scope;
+NAN_METHOD(Add) {
+  NanScope();
 
   if (args.Length() < 2) {
     ThrowException(Exception::TypeError(String::New("Wrong number of arguments")));
-    return scope.Close(Undefined());
+    NanReturnUndefined();
   }
 
   if (!args[0]->IsNumber() || !args[1]->IsNumber()) {
     ThrowException(Exception::TypeError(String::New("Wrong arguments")));
-    return scope.Close(Undefined());
+    NanReturnUndefined();
   }
 
   Local<Number> num = Number::New(args[0]->NumberValue() +
       args[1]->NumberValue());
-  return scope.Close(num);
+  NanReturnValue(num);
 }
 
 void Init(Handle<Object> exports) {
