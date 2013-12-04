6dda2e9dc85818271bad40de0ea952da72170c59
Matteo Collina <matteo.collina@gmail.com>
2013-12-04 12:19:13 +0100

    Adapted 1_hello_world to nan.

--- 1_hello_world/hello.cc
+++ 1_hello_world/hello.cc
@@ -1,16 +1,19 @@
 #include <node.h>
 #include <v8.h>
+#include "nan.h"
+#include "hello.h"
 
 using namespace v8;
 
-Handle<Value> Method(const Arguments& args) {
-  HandleScope scope;
-  return scope.Close(String::New("world"));
+NAN_METHOD(Hello) {
+  NanScope();
+
+  NanReturnValue(String::New("world"));
 }
 
 void init(Handle<Object> exports) {
   exports->Set(String::NewSymbol("hello"),
-      FunctionTemplate::New(Method)->GetFunction());
+      FunctionTemplate::New(Hello)->GetFunction());
 }
 
 NODE_MODULE(hello, init)
