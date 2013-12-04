b67a339087bb05b82168d242f35b06113a88984a
Matteo Collina <matteo.collina@gmail.com>
2013-12-04 12:30:43 +0100

    Updated 4_object_factory.

--- 4_object_factory/addon.cc
+++ 4_object_factory/addon.cc
@@ -1,15 +1,15 @@
-#define BUILDING_NODE_EXTENSION
 #include <node.h>
+#include "nan.h"
 
 using namespace v8;
 
-Handle<Value> CreateObject(const Arguments& args) {
-  HandleScope scope;
+NAN_METHOD(CreateObject) {
+  NanScope();
 
   Local<Object> obj = Object::New();
   obj->Set(String::NewSymbol("msg"), args[0]->ToString());
 
-  return scope.Close(obj);
+  NanReturnValue(obj);
 }
 
 void Init(Handle<Object> exports, Handle<Object> module) {
