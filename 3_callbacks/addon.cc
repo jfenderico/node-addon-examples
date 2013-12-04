d635e4bac67d645938942709e95331390bc64194
Matteo Collina <matteo.collina@gmail.com>
2013-12-04 12:28:23 +0100

    Adapted 3_callbacks.

--- 3_callbacks/addon.cc
+++ 3_callbacks/addon.cc
@@ -1,17 +1,17 @@
-#define BUILDING_NODE_EXTENSION
 #include <node.h>
+#include "nan.h"
 
 using namespace v8;
 
-Handle<Value> RunCallback(const Arguments& args) {
-  HandleScope scope;
+NAN_METHOD(RunCallback) {
+  NanScope();
 
   Local<Function> cb = Local<Function>::Cast(args[0]);
   const unsigned argc = 1;
   Local<Value> argv[argc] = { Local<Value>::New(String::New("hello world")) };
   cb->Call(Context::GetCurrent()->Global(), argc, argv);
 
-  return scope.Close(Undefined());
+  NanReturnUndefined();
 }
 
 void Init(Handle<Object> exports, Handle<Object> module) {
